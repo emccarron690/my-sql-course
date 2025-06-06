26b54a5afc778621677130f337a727ed78f771d3
emccarron690 <emma.mccarron@mandg.com>
2025-06-06 10:51:46 +0000

    first lesson

--- foundation-lesson.sql
+++ foundation-lesson.sql
@@ -4,6 +4,13 @@ SELECT
     , ps.DischargeDate 
     , ps.Hospital
     , ps.Ward
+    , ps.Tariff
 FROM PatientStay ps
 WHERE ps.Hospital IN ('Kingston' , 'PRUH')
-AND ps.Ward LIKE  '%Surgery'
+--AND ps.Ward LIKE  '%Surgery'
+AND ps.AdmittedDate >= '2024-03-01'
+AND ps.DischargeDate BETWEEN '2024-03-03' AND '2024-03-05'
+AND ps.Tariff > 7
+ORDER BY 
+    ps.AdmittedDate 
+    , ps.DischargeDate
