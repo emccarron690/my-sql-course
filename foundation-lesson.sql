a8cec7d24de2f6b02aa630045e4942a4f14da782
emccarron690 <emma.mccarron@mandg.com>
2025-06-06 14:45:30 +0000

    end of course

--- foundation-lesson.sql
+++ foundation-lesson.sql
@@ -1,5 +1,7 @@
-SELECT 
+SELECT
     ps.PatientId 
+    , DATEADD(DAY, -14, ps.AdmittedDate) AS ReminderDate
+    , DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) AS LengthofStay
     , ps.AdmittedDate 
     , ps.DischargeDate 
     , ps.Hospital
@@ -7,10 +9,39 @@ SELECT
     , ps.Tariff
 FROM PatientStay ps
 WHERE ps.Hospital IN ('Kingston' , 'PRUH')
---AND ps.Ward LIKE  '%Surgery'
-AND ps.AdmittedDate >= '2024-03-01'
-AND ps.DischargeDate BETWEEN '2024-03-03' AND '2024-03-05'
-AND ps.Tariff > 7
+    --AND ps.Ward LIKE  '%Surgery'
+    AND ps.AdmittedDate >= DATEFROMPARTS(2024,2,27)
+    AND ps.DischargeDate BETWEEN '2024-03-03' AND '2024-03-05'
+    AND ps.Tariff > 7
 ORDER BY 
     ps.AdmittedDate 
     , ps.DischargeDate
+
+
+SELECT
+    ps.Hospital
+    , COUNT(*) AS NumberOfPatients 
+    , SUM(ps.Tariff) AS TotalTariff
+    , AVG(ps.Tariff) AS AverageTariff
+    , SUM(ps.Tariff)/COUNT(*) AS Average2
+FROM PatientStay ps
+GROUP BY
+    ps.Hospital
+HAVING Count(*) >10
+ORDER BY NumberOfPatients DESC
+--ORDER BY ps.Hospital , ps.Ward
+
+SELECT *
+FROM DimHospital
+
+SELECT *
+FROM DimHospital
+
+SELECT
+    ps.PatientId
+    , h.Hospital
+    , ps.Hospital
+    , ps.AdmittedDate
+    , h.HospitalType
+    , h.HospitalSize
+FROM PatientStay ps FULL OUTER JOIN DimHospitalBad h ON ps.Hospital = h.Hospital
